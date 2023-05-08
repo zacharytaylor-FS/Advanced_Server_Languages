5632df016fb79901dfd9328ccd743f09c5bd210c
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-05-08 03:27:57 -0400

    Add GET and POST to /, and a GET for pages

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const app = express();
 
 express.urlencoded({extended: true})
 
+app.get('/', (req, res) => {
+    res.send(`Welcome to the HOME PAGE GET`)
+})
+
+app.post('/', (req, res, next) => {
+    res.send(`Home Page POST`)
+})
 
 app.get('/products/all', (req, res) => {
     res.send(`GET Products: ${req.get('Page')}, ${req.get('Sort')}, ${req.get('Order')}`)
 })
 
-app.get('/products/:id-:size-:color', (req, res) => {
+app.get('/products/:productId-:productSize-:productColor', (req, res) => {
     const id = 
-    res.send(`GET Products: ${req.params.id}, ${req.params.size}, ${req.params.color}`)
+    res.send(`GET Products: Id - ${req.params.productId}, Size - ${req.params.productSize}, Color - ${req.params.productColor}`)
 })
 
 app.get('/products/:id', (req, res) => {
     res.send(`GET Products: ${req.params.id}`)
 })
 
+app.get(/^\/pages\/([0-9].*)/, (req, res) => {
+    res.send('Page: ' + req.params.pageId)
+})
 
 app.listen(3000)
