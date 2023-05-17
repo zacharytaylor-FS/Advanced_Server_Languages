4d8f8b70468ebf9b438a1d41a8c18d7966cc1b17
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-05-17 18:42:41 -0400

    Setup error handling

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+require('dotenv').config()
 
 const app = express();
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'pug')
-
 app.set('views', __dirname + '/templates')
+
 
 app.get('/', (req, res, next) => {
     res.render('views/products/index', {})
 })
 
-app.listen(8080, () => {
-    console.log('Express server running on port: 8080')
+app.use((req,res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.render('views/products/error')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Express server running on port: ${process.env.PORT}`)
 })
