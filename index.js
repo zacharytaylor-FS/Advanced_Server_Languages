c022366dda3ddb9f2ff386d20185270bc965198e
zacharytaylor-FS <ZJTaylor@student.fullsail.edu>
2023-05-17 21:21:55 -0400

    Error 404

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.get('/', (req, res, next) => {
     res.render('views/products/index', {})
 })
 
+//* Catch 404
+app.use((req, res, next) => {
+   next(createError(404))
+})
+
+//* Error Handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+
+    res.status(err.status || 500);
+    res.render('views/products/error', {})
+})
+
 app.listen(8080, () => {
     console.log('Express server running on port: 8080')
 })
